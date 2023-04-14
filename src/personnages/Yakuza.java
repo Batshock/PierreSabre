10dfc90dd710fc7d89ceb520f660820a1d11798f
Dimitri Kouzmitch <kzd4327a@ens.fsi>
2023-04-14 09:37:03 +0200

    Création de la classe Yakuza

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -2,12 +2,14 @@ package personnages;
 
 public class Yakuza extends Humain {
 	private String clan;
+	private int reputation = 0;
 	public Yakuza(String nom, String clan, String boissonPref, int argent) {
 		super(nom,boissonPref,argent);
 		this.clan = clan;
 	}
 	public void extorquer(Commercant victime) {
 		int gain = victime.getArgent();
+		reputation += 1;
 		gagnerArgent(gain);
 		victime.seFaireExtorquer();
 		parler("J'ai piqué les "+gain+" sous de "+victime.getNom()
